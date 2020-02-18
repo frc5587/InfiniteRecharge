9d300c0e122eb2218e2dee36871471a21caa679b
chw9 <43727884+chw9@users.noreply.github.com>
2020-02-18 17:06:52 -0500

    removed duplicate constants class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -38,10 +38,6 @@ public final class Constants {
     public static final int CONVEYOR_MOTOR = 10;
   }
 
-  public static final class ConveyorConstants {
-    public static final int CONVEYOR_MOTOR = 10;
-  }
-
     public static final class ArmConstants {
         public static final int ARM_MOTOR = 30;
 
