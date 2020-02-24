ccd8c6a7291363c901ddd38d215acc754ee91b9c
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2020-02-24 09:16:54 -0500

    Python script to scan files

---
+++ test.py
@@ -0,0 +1,16 @@
+import os
+
+print("root:")
+print(os.listdir("."))
+
+print("src\\main\\java:")
+print(os.listdir(".\\src\\main\\java"))
+
+print("src\\main\\java\\org:")
+print(os.listdir(".\\src\\main\\java"))
+
+print("src\\main\\java\\org\\frc5587:")
+print(os.listdir(".\\src\\main\\java\\org\\frc5587"))
+
+print("src\\main\\java\\org\\frc5587\\lib:")
+print(os.listdir(".\\src\\main\\java\\org\\frc5587\\lib"))
