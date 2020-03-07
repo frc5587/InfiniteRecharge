ca5cd0fc388920dc1cedfbc8de1f90d083208acf
chw9 <43727884+chw9@users.noreply.github.com>
2020-03-07 11:38:21 -0500

    added two auto CommandGroups

---
+++ src/main/java/frc/robot/commands/autonomous/FrontOfLoadingBay.java
@@ -0,0 +1,28 @@
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.ArmThread;
+import frc.robot.commands.AutoIntake;
+import frc.robot.commands.FindTarget;
+import frc.robot.commands.LimelightCentering;
+import frc.robot.commands.RamseteCommandWrapper;
+import frc.robot.commands.ShooterThread;
+import frc.robot.commands.RamseteCommandWrapper.AutoPaths;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Shooter;
+
+public class FrontOfLoadingBay extends SequentialCommandGroup {
+    public FrontOfLoadingBay(Arm arm, Conveyor conveyor, Drivetrain drivetrain, Intake intake, Limelight limelight, Shooter shooter, AutoPaths reverseToRendezvous, AutoPaths sCurveFromRendezvous, FindTarget findTarget, ArmThread armThread, ShooterThread shooterThread) {
+        addCommands(
+                new ParallelCommandGroup(new RamseteCommandWrapper(drivetrain, reverseToRendezvous),
+                        new AutoIntake(intake, conveyor)),
+                new RamseteCommandWrapper(drivetrain, sCurveFromRendezvous), 
+                new ParallelCommandGroup(new LimelightCentering(drivetrain, limelight),
+                        new SequentialCommandGroup(findTarget, new ParallelCommandGroup(armThread, shooterThread))));
+    }
+}
